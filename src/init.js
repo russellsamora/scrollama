d1a218467a9c2d446803722b68c724c3ad0c3ba7
Andy Song <andysongs@qq.com>
2019-08-14 01:59:35 +0800

    revise comment

--- src/init.js
+++ src/init.js
@@ -258,7 +258,7 @@ function scrollama() {
     const ss = stepStates[index];
 
     // entering below is only when bottomAdjusted is positive
-    // and topAdjusted is positive
+    // and topAdjusted is negative
     if (
       isIntersecting &&
       topAdjusted <= 0 &&
